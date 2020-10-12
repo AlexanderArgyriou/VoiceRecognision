04db0a4b51167454b695d405a924acebd6388d0f
AlexanderArgyriou <67802974+AlexanderArgyriou@users.noreply.github.com>
2020-10-12 11:27:07 +0300

    Add files via upload

---
+++ VoiceRec/VoiceRec/VoiceRecognitionForm.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace VoiceRec
+{
+    partial class VoiceRecognitionForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(VoiceRecognitionForm));
+            this.TBox = new System.Windows.Forms.RichTextBox();
+            this.Enable = new System.Windows.Forms.Button();
+            this.Disable = new System.Windows.Forms.Button();
+            this.TextRadio = new System.Windows.Forms.RadioButton();
+            this.VoiceRadio = new System.Windows.Forms.RadioButton();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.TrainButton = new System.Windows.Forms.Button();
+            this.Question = new System.Windows.Forms.TextBox();
+            this.Answer = new System.Windows.Forms.TextBox();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TBox
+            // 
+            this.TBox.Location = new System.Drawing.Point(3, 3);
+            this.TBox.Name = "TBox";
+            this.TBox.Size = new System.Drawing.Size(297, 253);
+            this.TBox.TabIndex = 0;
+            this.TBox.Text = "";
+            // 
+            // Enable
+            // 
+            this.Enable.Location = new System.Drawing.Point(3, 262);
+            this.Enable.Name = "Enable";
+            this.Enable.Size = new System.Drawing.Size(125, 23);
+            this.Enable.TabIndex = 1;
+            this.Enable.Text = "Enable";
+            this.Enable.UseVisualStyleBackColor = true;
+            this.Enable.Click += new System.EventHandler(this.Enable_Click);
+            // 
+            // Disable
+            // 
+            this.Disable.Location = new System.Drawing.Point(3, 291);
+            this.Disable.Name = "Disable";
+            this.Disable.Size = new System.Drawing.Size(125, 23);
+            this.Disable.TabIndex = 2;
+            this.Disable.Text = "Disable";
+            this.Disable.UseVisualStyleBackColor = true;
+            this.Disable.Click += new System.EventHandler(this.Disable_Click);
+            // 
+            // TextRadio
+            // 
+            this.TextRadio.AutoSize = true;
+            this.TextRadio.Checked = true;
+            this.TextRadio.Location = new System.Drawing.Point(134, 262);
+            this.TextRadio.Name = "TextRadio";
+            this.TextRadio.Size = new System.Drawing.Size(76, 17);
+            this.TextRadio.TabIndex = 3;
+            this.TextRadio.TabStop = true;
+            this.TextRadio.Text = "Text Reply";
+            this.TextRadio.UseVisualStyleBackColor = true;
+            // 
+            // VoiceRadio
+            // 
+            this.VoiceRadio.AutoSize = true;
+            this.VoiceRadio.Location = new System.Drawing.Point(134, 291);
+            this.VoiceRadio.Name = "VoiceRadio";
+            this.VoiceRadio.Size = new System.Drawing.Size(82, 17);
+            this.VoiceRadio.TabIndex = 4;
+            this.VoiceRadio.Text = "Voice Reply";
+            this.VoiceRadio.UseVisualStyleBackColor = true;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.TBox);
+            this.flowLayoutPanel1.Controls.Add(this.Enable);
+            this.flowLayoutPanel1.Controls.Add(this.TextRadio);
+            this.flowLayoutPanel1.Controls.Add(this.Disable);
+            this.flowLayoutPanel1.Controls.Add(this.VoiceRadio);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(306, 327);
+            this.flowLayoutPanel1.TabIndex = 5;
+            // 
+            // TrainButton
+            // 
+            this.TrainButton.Location = new System.Drawing.Point(3, 3);
+            this.TrainButton.Name = "TrainButton";
+            this.TrainButton.Size = new System.Drawing.Size(100, 23);
+            this.TrainButton.TabIndex = 5;
+            this.TrainButton.Text = "Train";
+            this.TrainButton.UseVisualStyleBackColor = true;
+            this.TrainButton.Click += new System.EventHandler(this.TrainButton_Click);
+            // 
+            // Question
+            // 
+            this.Question.Location = new System.Drawing.Point(3, 32);
+            this.Question.Name = "Question";
+            this.Question.Size = new System.Drawing.Size(247, 20);
+            this.Question.TabIndex = 7;
+            this.Question.Text = "Command";
+            // 
+            // Answer
+            // 
+            this.Answer.Location = new System.Drawing.Point(3, 58);
+            this.Answer.Name = "Answer";
+            this.Answer.Size = new System.Drawing.Size(247, 20);
+            this.Answer.TabIndex = 8;
+            this.Answer.Text = "Answer";
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.Controls.Add(this.TrainButton);
+            this.flowLayoutPanel2.Controls.Add(this.Question);
+            this.flowLayoutPanel2.Controls.Add(this.Answer);
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(324, 15);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(250, 100);
+            this.flowLayoutPanel2.TabIndex = 9;
+            // 
+            // VoiceRecognitionForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(581, 374);
+            this.Controls.Add(this.flowLayoutPanel2);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "VoiceRecognitionForm";
+            this.Text = "SpeechApp";
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.flowLayoutPanel2.ResumeLayout(false);
+            this.flowLayoutPanel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox TBox;
+        private System.Windows.Forms.Button Enable;
+        private System.Windows.Forms.Button Disable;
+        private System.Windows.Forms.RadioButton TextRadio;
+        private System.Windows.Forms.RadioButton VoiceRadio;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button TrainButton;
+        private System.Windows.Forms.TextBox Question;
+        private System.Windows.Forms.TextBox Answer;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+    }
+}
+
---
+++ VoiceRec/VoiceRec/VoiceRecognitionForm.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+using System.IO;
+
+namespace VoiceRec
+{
+    public partial class VoiceRecognitionForm : Form
+    {
+        private SpeechRecognitionEngine RecEng;
+        private GrammarBuilder             GrB;
+        private Grammar                      G;
+        private Dictionary<String, String>   D;
+        private Choices               Commands;
+        private SpeechSynthesizer        Synth;
+
+        public VoiceRecognitionForm()
+        {
+            InitializeComponent();
+            Init();
+        }   // Constructor
+
+        private void Init()
+        {
+            // Start with disabling some features.
+            Disable.Enabled = false;
+            Question.Enabled = false;
+            Answer.Enabled = false;
+
+            // Create an instance of a SpeechRecognitionEngine.
+            RecEng = new SpeechRecognitionEngine();
+
+            // Create an instance of a Dictionary of Strings, which will hold Tuples -> <Quest, Ans>.
+            D = new Dictionary<String, String>();
+
+            // Read all the Ans-Quest from the Grammar.
+            string[] Lines = System.IO.File.ReadAllLines("Grammar.txt");
+
+            // Create an instance of Choices and fill The Choice set with commands
+            Commands = new Choices();
+            for (int i = 0; i < Lines.Length; i++)
+            {
+                string[] Tokens = Lines[i].Split('\t');
+                if (Tokens[0] != "")
+                {
+                    Commands.Add(Tokens[0]); // Append the command.
+                    D[Tokens[0]] = Tokens[1]; // insert <Quest, Ans>. to the Dictionary.
+                }   // if
+            }   // for
+
+            // Create an instance of a grammar builder, and append the commands.
+            GrB = new GrammarBuilder();
+            GrB.Append(Commands);
+
+            // Create a Grammar grom the GrammarBuilder.
+            G = new Grammar(GrB);
+
+            // Load The Grammar to the Engine Recognision.
+            RecEng.LoadGrammarAsync(G);
+
+            // Set The input Device to default.
+            RecEng.SetInputToDefaultAudioDevice();
+
+            // Create an instance of a SpeechSynthesizer.
+            Synth = new SpeechSynthesizer();
+
+            // Configure the audio output.   
+            Synth.SetOutputToDefaultAudioDevice();
+
+            // Handle a SpeechRecognized event.
+            RecEng.SpeechRecognized += RecEng_SpeechRecognized;
+        }   // Init
+
+        private void RecEng_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            // Print The answer.
+            if (TextRadio.Checked) TBox.Text += D[e.Result.Text] + '\n';
+            else Synth.Speak(D[e.Result.Text]);
+        }   // RecEng_SpeechRecognized event handler 
+
+        private void Enable_Click(object sender, EventArgs e)
+        {
+            // Start listen and recognize.
+            RecEng.RecognizeAsync(RecognizeMode.Multiple);
+
+            // Switch buttons.
+            Enable.Enabled = false;
+            Disable.Enabled = true;
+        }   // Enable_Click
+
+        private void Disable_Click(object sender, EventArgs e)
+        {
+            // Stop listen and recognize.
+            RecEng.RecognizeAsyncStop();
+
+            // Switch buttons.
+            Enable.Enabled = true;
+            Disable.Enabled = false;
+        }   // Disable_Click
+
+        private void TrainButton_Click(object sender, EventArgs e)
+        {
+            Disable_Click(sender, e);
+            if (TrainButton.Text.Equals("Train"))
+            {
+                Question.Enabled = true;
+                Answer.Enabled = true;
+                TrainButton.Text = "Submit";
+                Enable.Enabled = false;
+            }   // if
+            else
+            {
+                if(!Question.Text.Equals("") 
+                    && !Answer.Text.Equals(""))
+                {
+                    File.AppendAllText("Grammar.txt",
+                        Question.Text + '\t' + Answer.Text + Environment.NewLine);
+                    Init();
+                }   // if
+
+                Question.Enabled = false;
+                Answer.Enabled = false;
+                TrainButton.Text = "Train";
+                Enable.Enabled = true;
+            }   // else
+
+            Question.Text = "Command";
+            Answer.Text = "Answer";
+        }   // TrainButton_Click
+    }   // VoiceRecognitionForm
+}   // VoiceRec
